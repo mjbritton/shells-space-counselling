41066f1b6a09dc9099994838f4d0a75855c3e221
Mark <mark@mjbritton.co.uk>
2023-01-21 13:42:36 +0000

    allow svg upload

--- functions.php
+++ functions.php
@@ -1,6 +1,31 @@
-<?php 
-	 add_action( 'wp_enqueue_scripts', 'mjb_enqueue_styles' );
-	 function mjb_enqueue_styles() {
- 		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
- 		  } 
- ?>
+<?php
+add_action('wp_enqueue_scripts', 'mjb_enqueue_styles');
+function mjb_enqueue_styles()
+{
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+}
+
+// Allow SVG
+add_filter('wp_check_filetype_and_ext', function ($data, $file, $filename, $mimes) {
+    global $wp_version;
+    if ($wp_version !== '4.7.1') {
+        return $data;
+    }
+    $filetype = wp_check_filetype($filename, $mimes);
+    return [
+        'ext'             => $filetype['ext'],
+        'type'            => $filetype['type'],
+        'proper_filename' => $data['proper_filename'],
+    ];
+}, 10, 4);
+function cc_mime_types($mimes)
+{
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+function fix_svg()
+{
+    echo '';
+}
+add_action('admin_head', 'fix_svg');
